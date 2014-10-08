10740f596f1086ea1c9db9c3c7be6bcf457728e1
Joseph Victor <jvictor@stanford.edu>
2014-10-08 07:03:01 -0700

    Added a password, because security thats why. Also a quit command and tested every path in the sever loop.

--- tiara/server.py
+++ tiara/server.py
@@ -4,9 +4,10 @@ import time
 import select
 import socket
 import Queue
+import os
 
 def HandleInput(g_data, inp):
-    inp = inp.strip()
+    inp = inp
     if inp == 'reply':
         g_data.SocialLogic().Reply()
         return "ok"
@@ -35,6 +36,13 @@ if __name__ == '__main__':
 
     message_queues = {}
 
+    server_passowrd = ""
+    abs_prefix = os.path.join(os.path.dirname(__file__), "../data")
+    with open(abs_prefix + '/server_password',"r") as f:
+        server_password = f.readline()
+
+    assert server_password != ""
+
     while inputs:
 
         g_data.SocialLogic().Act()
@@ -47,16 +55,26 @@ if __name__ == '__main__':
                 g_data.TraceInfo('new connection from %s:%s' %  client_address)
                 connection.setblocking(0)
                 inputs.append(connection)
-                message_queues[connection] = Queue.Queue()
+                message_queues[connection] = (Queue.Queue(),False)
             else:
                 data = s.recv(1024)
-                if data:
+                data = data.strip()
+                if data and data != "quit":
                     g_data.TraceInfo('received "%s" from %s' % (data, s.getpeername()))
-                    message_queues[s].put(HandleInput(g_data, data))
+                    if message_queues[s][1]:
+                        message_queues[s][0].put(HandleInput(g_data, data))
+                    else:
+                        if data == server_password:
+                            message_queues[s] = (message_queues[s][0],True)
+                            message_queues[s][0].put("welcome")
+                            g_data.TraceInfo('%s:%s entered the password' % s.getpeername())
+                        else:
+                            message_queues[s][0].put("password denied")
+                            g_data.TraceInfo('%s:%s failed the password' % s.getpeername())
                     if s not in outputs:
                         outputs.append(s)
                 else:
-                    g_data.TraceInfo('closing %s' % s.getpeername())
+                    g_data.TraceInfo('closing %s:%s' % s.getpeername())
                     if s in outputs:
                         outputs.remove(s)
                     inputs.remove(s)
@@ -64,14 +82,14 @@ if __name__ == '__main__':
                     del message_queues[s]
         for s in writable:
             try:
-                next_msg = message_queues[s].get_nowait()
+                next_msg = message_queues[s][0].get_nowait()
             except Queue.Empty:
                 outputs.remove(s)
             else:
                 g_data.TraceInfo('sending "%s" to %s' % (next_msg, s.getpeername()))
                 s.send(next_msg)
         for s in exceptional:
-            g_data.TraceInfo('closing %s' % s.getpeername())
+            g_data.TraceInfo('closing %s:%s' % s.getpeername())
             inputs.remove(s)
             if s in outputs:
                 outputs.remove(s)
